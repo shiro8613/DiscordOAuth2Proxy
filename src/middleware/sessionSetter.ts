7712d1353f80f73b2426a7f35ebd699105c24d29
flan0910 <psp1070@gmail.com>
2023-03-04 02:25:16 +0900

    session middleware

--- src/middleware/sessionSetter.ts
+++ src/middleware/sessionSetter.ts
@@ -1,4 +1,8 @@
-const session = require("express-session");
+import RedisStore from "connect-redis";
+import { RequestHandler } from "express";
+import session from "express-session";
+import { Redis } from "ioredis";
+import { Session as SessionTypes } from "../config/types";
 
 declare module 'express-session' {
     interface SessionData {
@@ -7,6 +11,14 @@ declare module 'express-session' {
     }
   }
 
-export function SessionSetter() {
-    
+export function SessionSetter(sessionOption :SessionTypes, redis :Redis, secret :string) :RequestHandler{
+    const redisStore = new RedisStore({ client: redis });
+   
+    return session( {
+        secret: secret,
+        cookie: {
+            ...sessionOption
+        },
+        store: redisStore       
+    } );
 }
