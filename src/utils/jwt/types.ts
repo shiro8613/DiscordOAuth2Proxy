20ccc78d0af6654eb71c909737c21fccb12cee80
toms0910 <psp1070@gmail.com>
2023-03-06 01:51:13 +0900

    なぜundefinedではないと確信していた....？ すべての可能性を考慮しろ！

--- src/utils/jwt/types.ts
+++ src/utils/jwt/types.ts
@@ -1,21 +1,21 @@
 
 
-type DiscordUser = {
-    username :string
-    nick :string
-    discriminator :string
+export type DiscordUser = {
+    username? :string
+    nick? :string
+    discriminator? :string
 }
 
-type DiscordData = {
-    discordLogin :boolean
-    disocrdJoinGuild :boolean
-    discordUser :DiscordUser
-    discordRoles :string[]
+export type DiscordData = {
+    discordLogin? :boolean
+    disocrdJoinGuild? :boolean
+    discordUser? :DiscordUser
+    discordRoles? :string[]
 }
 
 
 export type jwtData = {
-    discord :DiscordData
-    ip :string
-    loginTime :string
+    discord? :DiscordData
+    ip? :string
+    loginTime? :string
 }
