14efad9297208c0b1759ffb02a4d95d5e626a05d
toms0910 <psp1070@gmail.com>
2023-03-06 01:49:37 +0900

    const群

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
-export const DISCORD_LOGIN = "/login";
-export const DISCORD_AFTER = "/after";
-export const SLASH_HANDLER = "SlashSlashMan"
+export const DISCORD_LOGIN  = "/login";
+export const DISCORD_LOGOUT = "/logout";
+export const DISCORD_AFTER  = "/after";
+export const SLASH_HANDLER  = "SlashSlashMan"
